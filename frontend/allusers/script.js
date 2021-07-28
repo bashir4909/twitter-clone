5ef7dc1b469dac2e1825c642223bf9018702970a
Balagardash Bashirov <azbalagardashbashirov@gmail.com>
2021-07-28 22:40:28 +0100

    refactoring

--- frontend/allusers/script.js
+++ frontend/allusers/script.js
@@ -9,74 +9,58 @@ function readCookies() {
   return cookies
 }
 
+function mkButton(posturl, username, btntext) {
+  let btn = document.createElement("button")
+  btn.addEventListener("click", (evt) => {
+    fetch(`/api/v1/uid/${username}`)
+      .then(row => row.json())
+      .then(row => row.rowid)
+      .then(followingid => {
+        console.log(followingid)
+        fetch(posturl, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              "followingid": followingid,
+              "followerid": readCookies()["userid"]
+            })
+          })
+          .then((res) => {
+            window.location.reload()
+          })
+      })
+  })
+  btn.innerText = btntext
+  btn.classList.add("button")
+  btn.classList.add("is-small")
+  btn.classList.add("is-rounded")
+  btn.classList.add("media-right")
+  return btn
+}
+
+
 fetch("/api/v1/allusers")
   .then(rows => rows.json())
   .then(rows => {
-    console.log(rows)
     rows.forEach((row) => {
+
+      let li = document.createElement("li")
+      li.classList.add("media")
+
       let a = document.createElement("a")
       a.setAttribute("href", `/user?username=${row.username}`)
       a.innerText = "@" + row.username
-      let span = document.createElement("span")
+      a.classList.add("media-left")
 
-      let li = document.createElement("li")
       li.appendChild(a)
-      li.appendChild(span)
         // Next part works, but need a lot of refactoring
       if (readCookies()["userid"]) {
         if (row.isfollow) {
-          let fwbutton = document.createElement("button")
-          fwbutton.addEventListener("click", (evt) => {
-            fetch(`/api/v1/uid/${row.username}`)
-              .then(row => row.json())
-              .then(row => row.rowid)
-              .then((followingid) => {
-                let data = {
-                  "followerid": readCookies()["userid"],
-                  "followingid": followingid
-                }
-                console.log(data)
-                fetch("/api/v1/unfollow", {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify(data)
-                }).then((res) => {
-                  window.location.reload()
-                })
-              })
-          })
-          span.innerText = "Following"
-          fwbutton.innerText = "Unfollow"
-
-          li.appendChild(fwbutton)
+          li.appendChild(mkButton('/api/v1/unfollow', row.username, "Unfollow"))
         } else {
-          let fwbutton = document.createElement("button")
-          fwbutton.addEventListener("click", (evt) => {
-            fetch(`/api/v1/uid/${row.username}`)
-              .then(row => row.json())
-              .then(row => row.rowid)
-              .then((followingid) => {
-                let data = {
-                  "followerid": readCookies()["userid"],
-                  "followingid": followingid
-                }
-                console.log(data)
-                fetch("/api/v1/follow", {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify(data)
-                }).then((res) => {
-                  window.location.reload()
-                })
-              })
-          })
-          span.innerText = "Not following"
-          fwbutton.innerText = "Follow"
-          li.appendChild(fwbutton)
+          li.appendChild(mkButton('/api/v1/follow', row.username, "Follow"))
         }
       }
 
