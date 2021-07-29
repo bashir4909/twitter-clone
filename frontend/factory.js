cb13e234440e31e4dd494f8282e5045b7c6d4b29
Balagardash Bashirov <azbalagardashbashirov@gmail.com>
2021-07-29 10:48:51 +0100

    newtweet,twetview better modularity

---
+++ frontend/factory.js
@@ -0,0 +1,77 @@
+function mkUserView(username, fullname, bio) {
+  let card = document.createElement("div")
+  card.classList.add("card")
+  card.innerHTML = `
+    <div class="card-header">
+      <div class="card-header-title">
+        <p>${fullname}</p>
+      </div>
+      <small>@${username}</small>
+    </div>
+    <div class="card-content">
+      <p>${bio}</p>
+    </div>
+    `
+  return card
+}
+
+function mkTweetView(username, tweetcontent) {
+  let media = document.createElement("div")
+  media.classList.add("media")
+  media.innerHTML = `
+        <div class="media-left">
+          <p class="image is-64x64">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </p>
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <div class="columns">
+              <div class="column">
+                <p><a href="/user?username=${username}">@${username}:</a> ${tweetcontent}</p>
+              </div>
+              <div class="column is-narrow">
+                <button class="button">rt</button>
+                <button class="button">rp</button>
+                <button class="button">qt</button>
+              </div>
+
+            </div>
+          </div>
+        </div>
+    `
+  return media
+}
+
+function mkNewTweetForm() {
+  let lvl = document.createElement("div")
+  lvl.classList.add("level")
+  lvl.innerHTML = `
+    <form action="/api/v1/newtweet" method='POST'>
+      <div class="level-left">
+        <textarea name="tweettext" cols="35" rows="2" placeholder="Enter your tweet" class="textarea has-fixed-size"></textarea>
+      </div>
+      <div class="level-right">
+        <input type="submit" value="Tweet" class="button is-primary is-light is-rounded">
+      </div>
+    </form>
+  `
+  return lvl
+}
+
+function readCookies() {
+  let cookies = {}
+  document.cookie.split(";").forEach((pair) => {
+    let kv = pair.split("=")
+    cookies[kv[0].trim()] = kv[1]
+  })
+  return cookies
+}
+
+// document.querySelector(".columns > div").appendChild(
+//   mkUserView("sissy", "mamamio", "go fuck yourself")
+// )
+
+// document.querySelector("#tweets > div").appendChild(
+//   mkTweetView("bashir", "I have not tweeted for a while and my fingers are itching")
+// )
--- frontend/home/script.js
+++ frontend/home/script.js
@@ -1,49 +1,23 @@
-function readCookies() {
-  let cookies = {}
-  document.cookie.split(";").forEach((pair) => {
-    let kv = pair.split("=")
-    cookies[kv[0].trim()] = kv[1]
-  })
-  return cookies
-}
-
-function appendTw(twUsername, twContent) {
-  let dt = document.createElement("dt")
-  let a = document.createElement("a")
-  a.setAttribute("href", `/userpage?username=${twUsername}`)
-  a.innerText = "@" + twUsername
-  dt.appendChild(a)
-  let dd = document.createElement("dd")
-  let p = document.createElement("p")
-  p.innerText = twContent
-  dd.appendChild(p)
-    // parent div for bulma media
-  let div = document.createElement("div")
-  div.classList.add("media")
-  dt.classList.add("media-left")
-  dd.classList.add("media-right")
-  p.classList.add("media-content")
-  div.appendChild(dt)
-  div.appendChild(dd)
-  twList.appendChild(div)
-}
-
-function populateTweets() {
-  fetch(`http://localhost:3000/api/v1/timeline`)
-    .then(rows => rows.json())
-    .then(rows => {
-      rows.forEach((row) => {
-        appendTw(row.username, row.content)
-      })
-    })
-}
 let userid = readCookies()["userid"]
 let loginform = document.querySelector("#login-form")
 let loginformcontainer = document.querySelector("#login-form-container")
 let timeline = document.querySelector("#timeline")
 let twList = document.querySelector("#timeline > dl")
-if (!userid) {
+
+if (!readCookies()["userid"]) {
   loginformcontainer.classList.add("is-active")
 } else {
-  populateTweets()
+  fetch(`http://localhost:3000/api/v1/timeline`)
+    .then(rows => rows.json())
+    .then(rows => {
+      rows.forEach((row) => {
+        // appendTw(row.username, row.content)
+        document.querySelector("#timeline").appendChild(
+          mkTweetView(row.username, row.content)
+        )
+      })
+    })
+  document.querySelector("#new-tweet-form").appendChild(
+    mkNewTweetForm()
+  )
 }
